638107e5ae69ca9493dd618e9d24cfd19e8ca0e7
wujiawu <gunsnakee@foxmail.com>
2016-01-26 17:35:46 +0800

    ecitic pay test

--- app/src/main/java/com/huipengpay/sdk/demo/android/MainActivity.java
+++ app/src/main/java/com/huipengpay/sdk/demo/android/MainActivity.java
@@ -2,9 +2,17 @@ package com.huipengpay.sdk.demo.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
-public class MainActivity extends Activity{
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+public class MainActivity extends Activity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +22,25 @@ public class MainActivity extends Activity{
     @Override
     protected void onStart() {
         super.onStart();
-        TextView viewById = (TextView)findViewById(R.id.text_view);
-        viewById.setText("Hello Word!");
     }
+
+    public void eciticClick(View view) {
+        final EditText payMoneyView = (EditText) findViewById(R.id.payMoney);
+
+        String url = "http://192.168.8.254:8080/pay";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("pay_interface","ECITIC_APP");
+        map.put("order_describe","abc");
+        map.put("order_amount",new BigDecimal(payMoneyView.getText().toString()));
+
+        String result = restTemplate.postForObject(url,map,String.class);
+
+    }
+
+
 }
