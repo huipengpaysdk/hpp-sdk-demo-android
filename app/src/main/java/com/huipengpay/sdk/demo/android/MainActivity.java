b645e8f1d1c9a80383fd081f2fc9c8e550249a63
wujiawu <gunsnakee@foxmail.com>
2016-02-01 16:40:04 +0800

    WEIXIN_APP 支付

--- app/src/main/java/com/huipengpay/sdk/demo/android/MainActivity.java
+++ app/src/main/java/com/huipengpay/sdk/demo/android/MainActivity.java
@@ -2,13 +2,18 @@ package com.huipengpay.sdk.demo.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import com.alibaba.fastjson.JSON;
 import com.citicbank.cyberpay.assist.main.CyberPay;
 import com.citicbank.cyberpay.assist.main.CyberPayListener;
+import com.tencent.mm.sdk.constants.ConstantsAPI;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.modelpay.PayReq;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -18,55 +23,110 @@ import java.util.Map;
 
 public class MainActivity extends Activity {
 
-    private CyberPay mCyberPay;
-    private EditText payMoneyView;
+    private IWXAPI wxapi;
+    EditText payMoneyView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        mCyberPay = new CyberPay(getApplication());
         payMoneyView = (EditText) findViewById(R.id.payMoney);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     public void eciticClick(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                String postForObject = postForObject("ECITIC_APP");
+
+                Assert.notNull(postForObject);
+
+                goToCyberPay(JSON.parseObject(postForObject,Map.class));
+            }
+        }).start();
+    }
+
+    private String postForObject(String payInterface){
+
         String url = "http://192.168.8.254:8080/pay";
 
         RestTemplate restTemplate = new RestTemplate();
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-        map.add("pay_interface", "ECITIC_APP");
-        map.add("order_describe", "abc");
-        map.add("order_amount", payMoneyView.getText().toString());
+        MultiValueMap<String,String> map = new LinkedMultiValueMap<String,String>();
+        map.add("pay_interface",payInterface);
+        map.add("order_describe","abc");
+        map.add("order_amount",payMoneyView.getText().toString());
 
-        String postForObject = restTemplate.postForObject(url, map, String.class);
+        try{
 
-        goToCyberPay(JSON.parseObject(postForObject, Map.class));
+            return restTemplate.postForObject(url, map, String.class);
+
+        }catch (Exception ex){
+            Log.e("123456789",ex.getMessage(),ex);
+        }
+
+        return null;
     }
 
-
-    private void goToCyberPay(Map payResponse) {
+    private void goToCyberPay(Map payResponse){
         CyberPayListener cyberPayListener = new CyberPayListener() {
             public void onPayEnd(String s) {
-                Message msg = new Message();
-                Bundle data = new Bundle();
-                data.putString("value", s);
-                msg.setData(data);
+                Log.d("CyberPayEnd...",s);
             }
         };
-        mCyberPay.registerCallback(cyberPayListener);
-        Map extra = (Map) payResponse.get("extra");
-        Map<String, String> payRequest = new HashMap<>();
-        payRequest.put("MERID", (String) extra.get("MERID"));
-        payRequest.put("ORDERNO", (String) extra.get("ORDERNO"));
+        CyberPay cyberPay = new CyberPay(getApplication());
+        cyberPay.registerCallback(cyberPayListener);
+        Map extra =  (Map)payResponse.get("extra");
+        Map<String,String> payRequest = new HashMap<String,String>();
+        payRequest.put("MERID",(String) extra.get("MERID"));
+        payRequest.put("ORDERNO",(String) extra.get("ORDERNO"));
 
-        mCyberPay.pay(this, JSON.toJSONString(payRequest));
+        cyberPay.pay(this,JSON.toJSONString(payRequest));
     }
 
+    public void winxinClick(View view){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String postForObject = postForObject("WEIXIN_APP");
+                Assert.notNull(postForObject);
+                goToWeinxinPay(JSON.parseObject(postForObject,Map.class));
+            }
+        }).start();
+    }
 
-    Handler handler = new Handler() {
+    private void goToWeinxinPay(Map payResponse){
+        Log.d("goToWeinxinPay",JSON.toJSONString(payResponse));
+        Map extra =  (Map)payResponse.get("extra");
+        wxapi = WXAPIFactory.createWXAPI(MainActivity.this,null);
+        wxapi.registerApp((String)extra.get("appid"));
+        PayReq request = new PayReq();
+        request.appId = (String)extra.get("appid");
+        request.partnerId = (String) extra.get("partnerid");
+        request.prepayId = (String) extra.get("prepayid") ;
+        request.packageValue = (String) extra.get("package") ;
+        request.nonceStr = (String) extra.get("noncestr");
+        request.timeStamp = (String) extra.get("timestamp");
+        request.sign = (String) extra.get("sign");
+        wxapi.sendReq(request);
 
-    };
+    }
+
+    /**
+     * 微信回调
+     * @param resp
+     */
+    public void onResp(BaseResp resp){
+        if(resp.getType()== ConstantsAPI.COMMAND_PAY_BY_WX){
+            Log.d("weixin call back","1234");
+        }
+    }
+
 
 }
